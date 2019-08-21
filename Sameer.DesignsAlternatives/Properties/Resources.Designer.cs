2843158859a38f04a2534d4b9560e6d49a690c99
Sameer Al Harbi <smrhrbi@gmail.com>
2019-08-21 05:04:41 +0300

    user manual

--- Sameer.DesignsAlternatives/Properties/Resources.Designer.cs
+++ Sameer.DesignsAlternatives/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace Sameer.DesignsAlternatives.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap info_128 {
+            get {
+                object obj = ResourceManager.GetObject("info_128", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap info_64 {
             get {
                 object obj = ResourceManager.GetObject("info_64", resourceCulture);
--- Sameer.DesignsAlternatives/frmMain.Designer.cs
+++ Sameer.DesignsAlternatives/frmMain.Designer.cs
@@ -33,13 +33,14 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnDesignsAlternatives = new System.Windows.Forms.Button();
             this.btnBestSubCriteria = new System.Windows.Forms.Button();
             this.btnSettings = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -76,6 +77,7 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
@@ -84,17 +86,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1124, 121);
             this.panel1.TabIndex = 2;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.pictureBox1.Image = global::Sameer.DesignsAlternatives.Properties.Resources.building;
-            this.pictureBox1.Location = new System.Drawing.Point(925, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(197, 119);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // label3
             // 
@@ -127,6 +118,17 @@
             this.toolTip1.IsBalloon = true;
             this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
             this.toolTip1.ToolTipTitle = "AD-DSS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox1.Image = global::Sameer.DesignsAlternatives.Properties.Resources.building;
+            this.pictureBox1.Location = new System.Drawing.Point(925, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // btnDesignsAlternatives
             // 
@@ -182,6 +184,16 @@
             this.btnSettings.UseVisualStyleBackColor = true;
             this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
             // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Image = global::Sameer.DesignsAlternatives.Properties.Resources.info_64;
+            this.button1.Location = new System.Drawing.Point(53, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 75);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -216,6 +228,7 @@
         private System.Windows.Forms.Button btnDesignsAlternatives;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnBestSubCriteria;
+        private System.Windows.Forms.Button button1;
     }
 }
 
